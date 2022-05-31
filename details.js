4ea4247b2d33265b02d480637b68708252288f83
pratham_s <pratham_s@anexgate.com>
2022-06-01 00:55:44 +0530

    deleted unwanted files

--- details.js
+++
@@ -1,8 +0,0 @@
-
-
-// console.log("detail ",document.getElementById('detail-container'))
-
-function details(data){
-    console.log("details function called",data)
-    console.log(document.getElementById('detail-container').length)
-}
